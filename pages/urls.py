bc0db7a52c2808e7b45610cdce163df61fbb7c1e
shakhan_ali <ali_shakhan@yahoo.com>
2020-11-07 13:18:09 +1030

    currency

--- pages/urls.py
+++ pages/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import HomePageView, AboutPageView
+from .views import HomePageView, AboutPageView, ExchangePageView
 
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
+    path('exchange', ExchangePageView.as_view(), name='exchange'),
     path('', HomePageView.as_view(), name='home'), 
     
 ]
