e85366c7edbb37d00517ae932d06df3f4549756b
shakhan_ali <ali_shakhan@yahoo.com>
2020-11-07 20:58:54 +1030

    ch12

--- config/settings.py
+++ config/settings.py
@@ -163,6 +163,9 @@ STATICFILES_FINDERS = [
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
 ]
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media')) 
+ 
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
